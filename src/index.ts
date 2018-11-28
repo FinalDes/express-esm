5c650bab23f0b45d0808d59c51523a368572bd73
Alex Alonso <alex4551@gmail.com>
2018-11-28 19:17:05 +0800

    add typescript express js demo commonjs

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, ()=> {
+    console.log('listening port 3000');
+});
